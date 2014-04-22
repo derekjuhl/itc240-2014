4c2c12a31fe51a446091553fe36ef368c730c731
derekjuhl <derekjuhl2001@yahoo.com>
2014-04-22 03:52:28 -0700

    Update header.php
    Changed header text to Dutch, edited code

--- assignment-01/header.php
+++ assignment-01/header.php
@@ -2,10 +2,10 @@
 <html>
   <head>
     <title><?php echo $page_title; ?></title>
-  </head>
-  <body>
-    <h1>HEADER OF ASSIGNMENT 01</h1>
-<?php
-echo $page_title;
-?>
+	</head>
+	<body>
+		<h1>HET HOOFDING</h1>
 
+<?php
+	echo $page_title;
+?>
